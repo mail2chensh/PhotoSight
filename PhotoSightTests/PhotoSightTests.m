eb1a34fc8670502ed0fa440da28ae230b6609196
chensh <mail2chensh@gmail.com>
2015-03-05 19:02:49 -0800

    Initial Commit

---
+++ PhotoSightTests/PhotoSightTests.m
@@ -0,0 +1,34 @@
+//
+//  PhotoSightTests.m
+//  PhotoSightTests
+//
+//  Created by chensh on 15-3-5.
+//  Copyright (c) 2015年 Chensh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PhotoSightTests : XCTestCase
+
+@end
+
+@implementation PhotoSightTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
